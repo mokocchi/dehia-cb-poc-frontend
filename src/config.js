db38cc5b8dddb048ac4c71a84be0888c73cbdfa5
Jose Arcidiacono <j2arcidiacono@outlook.com>
2021-03-19 08:40:14 -0300

    Parametrize gateway url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-export const API_HOST = ''
+import { GATEWAY_URL } from "./env";
 
-export const API_BASE_URL = `${API_HOST}/api/v1.0`; 
+export const API_HOST = GATEWAY_URL;
+
+export const API_BASE_URL = `${API_HOST}/api/v1.0`;
 
 export const TOKEN_AUTH_URL = `${API_HOST}/login`;
 
